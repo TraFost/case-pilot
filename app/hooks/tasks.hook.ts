a0382fd8a86e349ea9ca9b02a8f477e07615ea3b
Rahman Nurudin <rahwisdilfiqrak@gmail.com>
2026-02-07 03:49:28 +0700

    chore: Remove unused favicon, SVG files, and task hooks

--- app/hooks/tasks.hook.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "convex/react";
-
-import { api } from "@/convex/_generated/api";
-
-export function useTasks() {
-	const tasks = useQuery(api.tasks.get);
-
-	return tasks;
-}
